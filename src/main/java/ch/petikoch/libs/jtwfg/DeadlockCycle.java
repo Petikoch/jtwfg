f2e4248fe6821642a476bed0e7bcc748d30de9bb
peti <petikoch@gmail.com>
2021-04-04 17:56:20 +0200

    various updates on project setup - java 8 instead of 7 - gradle 6.8.3 instead of 3.1 - remove jcenter/bintray, use maven central instead - simplify: remove jacoco + coveralls

--- src/main/java/ch/petikoch/libs/jtwfg/DeadlockCycle.java
+++ src/main/java/ch/petikoch/libs/jtwfg/DeadlockCycle.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014-2015 Peti Koch und Adrian Elsener
+ * Copyright 2014-2021 Peti Koch und Adrian Elsener
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,162 +21,157 @@ import java.util.*;
 
 /**
  * The representation of a cycle between tasks in a "task wait for model" graph.
- * <p/>
+ * <p>
  * Immutable / thread-safe.
  *
- * @param <T>
- * 		The type of the ID of the tasks. Something with a meaningful {@link Object#equals(Object)} and {@link
- * 		Object#hashCode()} implementation like {@link String}, {@link Long} or a class of your domain model which is fine
- * 		to use as a key e.g. in a {@link java.util.HashMap}. If T implements Comparable, then you get sorted collections.
+ * @param <T> The type of the ID of the tasks. Something with a meaningful {@link Object#equals(Object)} and {@link
+ *            Object#hashCode()} implementation like {@link String}, {@link Long} or a class of your domain model which is fine
+ *            to use as a key e.g. in a {@link java.util.HashMap}. If T implements Comparable, then you get sorted collections.
  */
 public class DeadlockCycle<T> {
 
-	private final List<T> cycleTasks;
-	private final Map<T, Set<T>> alsoDeadlockedTasks;
-	private final Set<T> allDeadlockedTasks;
+    private final List<T> cycleTasks;
+    private final Map<T, Set<T>> alsoDeadlockedTasks;
+    private final Set<T> allDeadlockedTasks;
 
-	DeadlockCycle(final List<T> cycleTasks, /* Nullable */ final Map<T, Set<T>> alsoDeadlockedTasks) {
-		Preconditions.checkArgument(cycleTasks != null && !cycleTasks.isEmpty(), "There are no cycle tasks: " + cycleTasks);
-		this.cycleTasks = Collections.unmodifiableList(cycleTasks);
-		if (null != alsoDeadlockedTasks) {
-			final LinkedHashMap<T, Set<T>> alsoDeadlockedTasksCopyMap = new LinkedHashMap<>(alsoDeadlockedTasks.size());
-			for (Map.Entry<T, Set<T>> mapEntry : alsoDeadlockedTasks.entrySet()) {
-				alsoDeadlockedTasksCopyMap.put(mapEntry.getKey(), Collections.unmodifiableSet(mapEntry.getValue()));
-			}
-			this.alsoDeadlockedTasks = Collections.unmodifiableMap(alsoDeadlockedTasksCopyMap);
-		} else {
-			this.alsoDeadlockedTasks = Collections.emptyMap();
-		}
-		Set<T> allInvolved = new LinkedHashSet<>(this.cycleTasks);
-		allInvolved.addAll(this.alsoDeadlockedTasks.keySet());
-		this.allDeadlockedTasks = Collections.unmodifiableSet(allInvolved);
-	}
+    DeadlockCycle(final List<T> cycleTasks, /* Nullable */ final Map<T, Set<T>> alsoDeadlockedTasks) {
+        Preconditions.checkArgument(cycleTasks != null && !cycleTasks.isEmpty(), "There are no cycle tasks: " + cycleTasks);
+        this.cycleTasks = Collections.unmodifiableList(cycleTasks);
+        if (null != alsoDeadlockedTasks) {
+            final LinkedHashMap<T, Set<T>> alsoDeadlockedTasksCopyMap = new LinkedHashMap<>(alsoDeadlockedTasks.size());
+            for (Map.Entry<T, Set<T>> mapEntry : alsoDeadlockedTasks.entrySet()) {
+                alsoDeadlockedTasksCopyMap.put(mapEntry.getKey(), Collections.unmodifiableSet(mapEntry.getValue()));
+            }
+            this.alsoDeadlockedTasks = Collections.unmodifiableMap(alsoDeadlockedTasksCopyMap);
+        } else {
+            this.alsoDeadlockedTasks = Collections.emptyMap();
+        }
+        Set<T> allInvolved = new LinkedHashSet<>(this.cycleTasks);
+        allInvolved.addAll(this.alsoDeadlockedTasks.keySet());
+        this.allDeadlockedTasks = Collections.unmodifiableSet(allInvolved);
+    }
 
-	/**
-	 * @return an unmodifiable list of the tasks which build the cycle
-	 */
-	@SuppressWarnings("UnusedDeclaration")
-	public List<T> getCycleTasks() {
-		return cycleTasks;
-	}
+    /**
+     * @return an unmodifiable list of the tasks which build the cycle
+     */
+    @SuppressWarnings("UnusedDeclaration")
+    public List<T> getCycleTasks() {
+        return cycleTasks;
+    }
 
-	/**
-	 * @return an unmodifiable map of other tasks outside of the cycle, which are direct or indirect dependent on a task
-	 * of the cycle
-	 */
-	public Map<T, Set<T>> getAlsoDeadlockedTasks() {
-		return alsoDeadlockedTasks;
-	}
+    /**
+     * @return an unmodifiable map of other tasks outside of the cycle, which are direct or indirect dependent on a task
+     * of the cycle
+     */
+    public Map<T, Set<T>> getAlsoDeadlockedTasks() {
+        return alsoDeadlockedTasks;
+    }
 
-	/**
-	 * @return an unmodifiable set of all deadlocked tasks because of this deadlock cycle
-	 */
-	public Set<T> getAllDeadlockedTasks() {
-		return allDeadlockedTasks;
-	}
+    /**
+     * @return an unmodifiable set of all deadlocked tasks because of this deadlock cycle
+     */
+    public Set<T> getAllDeadlockedTasks() {
+        return allDeadlockedTasks;
+    }
 
-	/**
-	 * @param task
-	 * 		not null
-	 *
-	 * @return true if the given task is deadlocked because of this deadlock cycle
-	 */
-	public boolean isDeadlocked(T task) {
-		Preconditions.checkArgumentNotNull(task, "task must not be null");
-		return allDeadlockedTasks.contains(task);
-	}
+    /**
+     * @param task not null
+     * @return true if the given task is deadlocked because of this deadlock cycle
+     */
+    public boolean isDeadlocked(T task) {
+        Preconditions.checkArgumentNotNull(task, "task must not be null");
+        return allDeadlockedTasks.contains(task);
+    }
 
-	/**
-	 * @param tasks
-	 * 		not null
-	 *
-	 * @return true if all the given tasks are deadlocked because of this deadlock cycle
-	 */
-	public boolean areAllDeadlocked(Iterable<T> tasks) {
-		Preconditions.checkArgumentNotNull(tasks, "tasks must not be null");
-		for (T task : tasks) {
-			if (!isDeadlocked(task)) {
-				return false;
-			}
-		}
-		return true;
-	}
+    /**
+     * @param tasks not null
+     * @return true if all the given tasks are deadlocked because of this deadlock cycle
+     */
+    public boolean areAllDeadlocked(Iterable<T> tasks) {
+        Preconditions.checkArgumentNotNull(tasks, "tasks must not be null");
+        for (T task : tasks) {
+            if (!isDeadlocked(task)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	@Override
-	public boolean equals(final Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
-		final DeadlockCycle<T> that = (DeadlockCycle<T>) o;
+        final DeadlockCycle<T> that = (DeadlockCycle<T>) o;
 
-		if (alsoDeadlockedTasks.equals(that.alsoDeadlockedTasks)) {
-			if (cycleTasks.size() == that.cycleTasks.size()) {
+        if (alsoDeadlockedTasks.equals(that.alsoDeadlockedTasks)) {
+            if (cycleTasks.size() == that.cycleTasks.size()) {
 
-				Set<T> thisTaskSet = new HashSet<>(cycleTasks);
-				Set<T> thatTaskSet = new HashSet<>(that.cycleTasks);
-				if (thisTaskSet.equals(thatTaskSet)) {
-					// the order is important
-					T firstElement = cycleTasks.iterator().next();
-					int indexOfFirstElementInThat = calculateIndex(that.cycleTasks, firstElement);
-					if (indexOfFirstElementInThat > 0) {
-						List<T> reorderedCopyOfThatInvolvedTasks = new ArrayList<>(that.cycleTasks.size());
-						reorderedCopyOfThatInvolvedTasks.addAll(that.cycleTasks.subList(indexOfFirstElementInThat, that.cycleTasks.size()));
-						if (indexOfFirstElementInThat > 1) {
-							reorderedCopyOfThatInvolvedTasks.addAll(that.cycleTasks.subList(1, indexOfFirstElementInThat));
-						}
-						reorderedCopyOfThatInvolvedTasks.add(that.cycleTasks.get(indexOfFirstElementInThat));
-						return cycleTasks.equals(reorderedCopyOfThatInvolvedTasks);
-					} else {
-						return cycleTasks.equals(that.cycleTasks);
-					}
-				}
-			}
-		}
+                Set<T> thisTaskSet = new HashSet<>(cycleTasks);
+                Set<T> thatTaskSet = new HashSet<>(that.cycleTasks);
+                if (thisTaskSet.equals(thatTaskSet)) {
+                    // the order is important
+                    T firstElement = cycleTasks.iterator().next();
+                    int indexOfFirstElementInThat = calculateIndex(that.cycleTasks, firstElement);
+                    if (indexOfFirstElementInThat > 0) {
+                        List<T> reorderedCopyOfThatInvolvedTasks = new ArrayList<>(that.cycleTasks.size());
+                        reorderedCopyOfThatInvolvedTasks.addAll(that.cycleTasks.subList(indexOfFirstElementInThat, that.cycleTasks.size()));
+                        if (indexOfFirstElementInThat > 1) {
+                            reorderedCopyOfThatInvolvedTasks.addAll(that.cycleTasks.subList(1, indexOfFirstElementInThat));
+                        }
+                        reorderedCopyOfThatInvolvedTasks.add(that.cycleTasks.get(indexOfFirstElementInThat));
+                        return cycleTasks.equals(reorderedCopyOfThatInvolvedTasks);
+                    } else {
+                        return cycleTasks.equals(that.cycleTasks);
+                    }
+                }
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	private int calculateIndex(final List<T> tasks, final T element) {
-		int index = 0;
-		for (T t : tasks) {
-			if (t.equals(element)) {
-				return index;
-			}
-			index++;
-		}
-		throw new IllegalStateException("Element " + element + " not found in " + tasks);
-	}
+    private int calculateIndex(final List<T> tasks, final T element) {
+        int index = 0;
+        for (T t : tasks) {
+            if (t.equals(element)) {
+                return index;
+            }
+            index++;
+        }
+        throw new IllegalStateException("Element " + element + " not found in " + tasks);
+    }
 
-	@Override
-	public int hashCode() {
-		return allDeadlockedTasks.hashCode();
-	}
+    @Override
+    public int hashCode() {
+        return allDeadlockedTasks.hashCode();
+    }
 
-	@Override
-	public String toString() {
-		String result = DeadlockCycle.class.getSimpleName();
-		result += ": ";
-		for (int i = 0; i < cycleTasks.size() - 1; i++) {
-			result += cycleTasks.get(i) + " -> ";
-		}
-		result += cycleTasks.get(cycleTasks.size() - 1);
-		if (!alsoDeadlockedTasks.isEmpty()) {
-			result += ". The following tasks are also deadlocked, because they are direct or indirect dependent on at least one of the tasks in the deadlock cycle: ";
-			for (Map.Entry<T, Set<T>> mapEntry : alsoDeadlockedTasks.entrySet()) {
-				for (T waitForTask : mapEntry.getValue()) {
-					result += mapEntry.getKey();
-					result += "->";
-					result += waitForTask;
-					result += " ";
-				}
-			}
-			result = result.trim();
-			result += ".";
-		}
-		return result;
-	}
+    @Override
+    public String toString() {
+        String result = DeadlockCycle.class.getSimpleName();
+        result += ": ";
+        for (int i = 0; i < cycleTasks.size() - 1; i++) {
+            result += cycleTasks.get(i) + " -> ";
+        }
+        result += cycleTasks.get(cycleTasks.size() - 1);
+        if (!alsoDeadlockedTasks.isEmpty()) {
+            result += ". The following tasks are also deadlocked, because they are direct or indirect dependent on at least one of the tasks in the deadlock cycle: ";
+            for (Map.Entry<T, Set<T>> mapEntry : alsoDeadlockedTasks.entrySet()) {
+                for (T waitForTask : mapEntry.getValue()) {
+                    result += mapEntry.getKey();
+                    result += "->";
+                    result += waitForTask;
+                    result += " ";
+                }
+            }
+            result = result.trim();
+            result += ".";
+        }
+        return result;
+    }
 }
--- src/main/java/ch/petikoch/libs/jtwfg/GraphBuilder.java
+++ src/main/java/ch/petikoch/libs/jtwfg/GraphBuilder.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014-2015 Peti Koch und Adrian Elsener
+ * Copyright 2014-2021 Peti Koch und Adrian Elsener
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,218 +25,193 @@ import java.util.TreeSet;
 /**
  * A builder class for graph instance creation. Can be used concurrently by different threads which create together the
  * graph.
- * <p/>
+ * <p>
  * Thread-safe.
  *
- * @param <T>
- * 		The type of the ID of the tasks. Something with a meaningful {@link Object#equals(Object)} and {@link
- * 		Object#hashCode()} implementation like {@link String}, {@link Long} or a class of your domain model which is fine
- * 		to use as a key e.g. in a {@link java.util.HashMap}. If T implements Comparable, then you get sorted collections.
+ * @param <T> The type of the ID of the tasks. Something with a meaningful {@link Object#equals(Object)} and {@link
+ *            Object#hashCode()} implementation like {@link String}, {@link Long} or a class of your domain model which is fine
+ *            to use as a key e.g. in a {@link java.util.HashMap}. If T implements Comparable, then you get sorted collections.
  */
 public class GraphBuilder<T> {
 
-	private final Map<T, Task<T>> taskMap = new LinkedHashMap<>();
+    private final Map<T, Task<T>> taskMap = new LinkedHashMap<>();
 
-	private final Object internalLock = new Object();
+    private final Object internalLock = new Object();
 
-	/**
-	 * Adds a task in the graph, if not yet present.
-	 *
-	 * @param taskId
-	 * 		not null
-	 *
-	 * @return the GraphBuilder instance itself
-	 *
-	 * @throws java.lang.IllegalArgumentException
-	 * 		in case of taskId is null
-	 */
-	public GraphBuilder<T> addTask(T taskId) {
-		synchronized (internalLock) {
-			getOrAddTaskRepresentator(taskId);
-		}
-		return this;
-	}
+    /**
+     * Adds a task in the graph, if not yet present.
+     *
+     * @param taskId not null
+     * @return the GraphBuilder instance itself
+     * @throws java.lang.IllegalArgumentException in case of taskId is null
+     */
+    public GraphBuilder<T> addTask(T taskId) {
+        synchronized (internalLock) {
+            getOrAddTaskRepresentator(taskId);
+        }
+        return this;
+    }
 
-	/**
-	 * Checks, it there exists a task with the givenTaskId
-	 *
-	 * @param taskId
-	 * 		not null
-	 *
-	 * @return boolean
-	 *
-	 * @throws java.lang.IllegalArgumentException
-	 * 		in case of taskId is null
-	 */
-	public boolean hasTask(T taskId) {
-		Preconditions.checkArgumentNotNull(taskId, "taskId must not be null");
-		boolean result;
-		synchronized (internalLock) {
-			result = taskMap.containsKey(taskId);
-		}
-		return result;
-	}
+    /**
+     * Checks, it there exists a task with the givenTaskId
+     *
+     * @param taskId not null
+     * @return boolean
+     * @throws java.lang.IllegalArgumentException in case of taskId is null
+     */
+    public boolean hasTask(T taskId) {
+        Preconditions.checkArgumentNotNull(taskId, "taskId must not be null");
+        boolean result;
+        synchronized (internalLock) {
+            result = taskMap.containsKey(taskId);
+        }
+        return result;
+    }
 
-	/**
-	 * Removes a task from the graph. Removes also of course all "incoming" "wait for" dependencies from other tasks
-	 * referencing this one.
-	 *
-	 * @param taskId
-	 * 		not null
-	 *
-	 * @return the GraphBuilder instance itself
-	 *
-	 * @throws java.lang.IllegalArgumentException
-	 * 		in case of taskId was not added before
-	 */
-	public GraphBuilder<T> removeTask(T taskId) {
-		Preconditions.checkArgumentNotNull(taskId, "taskId must not be null");
-		synchronized (internalLock) {
-			Task<T> toRemove = taskMap.get(taskId);
-			if (toRemove != null) {
-				taskMap.remove(taskId);
-				for (Task<T> otherTasks : taskMap.values()) {
-					otherTasks.removeWaitFor(toRemove);
-				}
-			} else {
-				throw new IllegalArgumentException("taskId " + taskId + " is unknown and can't be removed");
-			}
-		}
-		return this;
-	}
+    /**
+     * Removes a task from the graph. Removes also of course all "incoming" "wait for" dependencies from other tasks
+     * referencing this one.
+     *
+     * @param taskId not null
+     * @return the GraphBuilder instance itself
+     * @throws java.lang.IllegalArgumentException in case of taskId was not added before
+     */
+    public GraphBuilder<T> removeTask(T taskId) {
+        Preconditions.checkArgumentNotNull(taskId, "taskId must not be null");
+        synchronized (internalLock) {
+            Task<T> toRemove = taskMap.get(taskId);
+            if (toRemove != null) {
+                taskMap.remove(taskId);
+                for (Task<T> otherTasks : taskMap.values()) {
+                    otherTasks.removeWaitFor(toRemove);
+                }
+            } else {
+                throw new IllegalArgumentException("taskId " + taskId + " is unknown and can't be removed");
+            }
+        }
+        return this;
+    }
 
-	/**
-	 * @param taskIds
-	 * 		may be null or empty
-	 *
-	 * @return the GraphBuilder instance itself
-	 *
-	 * @throws java.lang.IllegalArgumentException
-	 * 		in case of taskId was not added before
-	 */
-	public GraphBuilder<T> removeTasks(Iterable<T> taskIds) {
-		if (null != taskIds) {
-			synchronized (internalLock) {
-				for (T taskId : taskIds) {
-					if (!taskMap.containsKey(taskId)) {
-						throw new IllegalArgumentException("taskId " + taskId + " is unknown and can't be removed. None of the given tasks " + taskIds + " were removed");
-					}
-				}
-				for (T taskId : taskIds) {
-					removeTask(taskId);
-				}
-			}
-		}
-		return this;
-	}
+    /**
+     * @param taskIds may be null or empty
+     * @return the GraphBuilder instance itself
+     * @throws java.lang.IllegalArgumentException in case of taskId was not added before
+     */
+    public GraphBuilder<T> removeTasks(Iterable<T> taskIds) {
+        if (null != taskIds) {
+            synchronized (internalLock) {
+                for (T taskId : taskIds) {
+                    if (!taskMap.containsKey(taskId)) {
+                        throw new IllegalArgumentException("taskId " + taskId + " is unknown and can't be removed. None of the given tasks " + taskIds + " were removed");
+                    }
+                }
+                for (T taskId : taskIds) {
+                    removeTask(taskId);
+                }
+            }
+        }
+        return this;
+    }
 
-	/**
-	 * Removes the "wait for" dependency (and only the "wait for", not the tasks itself).
-	 *
-	 * @param taskId
-	 * 		not null
-	 * @param waitingOnTaskId
-	 * 		not null
-	 *
-	 * @return the GraphBuilder instance itself
-	 */
-	public GraphBuilder<T> removeTaskWaitForDependency(T taskId, T waitingOnTaskId) {
-		synchronized (internalLock) {
-			Task<T> task = taskMap.get(taskId);
-			Preconditions.checkArgumentNotNull(task, "taskId " + taskId + " is unknown");
-			Task<T> waitingOnTask = taskMap.get(waitingOnTaskId);
-			Preconditions.checkArgumentNotNull(waitingOnTask, "taskId " + waitingOnTaskId + " is unknown");
-			boolean removed = task.removeWaitFor(waitingOnTask);
-			if (!removed) {
-				throw new IllegalArgumentException(taskId + " is existing but was not waiting on " + waitingOnTaskId);
-			}
-		}
-		return this;
-	}
+    /**
+     * Removes the "wait for" dependency (and only the "wait for", not the tasks itself).
+     *
+     * @param taskId          not null
+     * @param waitingOnTaskId not null
+     * @return the GraphBuilder instance itself
+     */
+    public GraphBuilder<T> removeTaskWaitForDependency(T taskId, T waitingOnTaskId) {
+        synchronized (internalLock) {
+            Task<T> task = taskMap.get(taskId);
+            Preconditions.checkArgumentNotNull(task, "taskId " + taskId + " is unknown");
+            Task<T> waitingOnTask = taskMap.get(waitingOnTaskId);
+            Preconditions.checkArgumentNotNull(waitingOnTask, "taskId " + waitingOnTaskId + " is unknown");
+            boolean removed = task.removeWaitFor(waitingOnTask);
+            if (!removed) {
+                throw new IllegalArgumentException(taskId + " is existing but was not waiting on " + waitingOnTaskId);
+            }
+        }
+        return this;
+    }
 
-	/**
-	 * Adds a couple of taskIds in the graph, if not yet present.
-	 *
-	 * @param taskIds
-	 * 		not null, may be empty
-	 *
-	 * @return the GraphBuilder instance itself
-	 */
-	public GraphBuilder<T> addTasks(Iterable<T> taskIds) {
-		synchronized (internalLock) {
-			for (T task : taskIds) {
-				getOrAddTaskRepresentator(task);
-			}
-		}
-		return this;
-	}
+    /**
+     * Adds a couple of taskIds in the graph, if not yet present.
+     *
+     * @param taskIds not null, may be empty
+     * @return the GraphBuilder instance itself
+     */
+    public GraphBuilder<T> addTasks(Iterable<T> taskIds) {
+        synchronized (internalLock) {
+            for (T task : taskIds) {
+                getOrAddTaskRepresentator(task);
+            }
+        }
+        return this;
+    }
 
-	/**
-	 * Adds an edge between two tasks in the graph, if not yet present.
-	 *
-	 * @param taskId
-	 * 		not null
-	 * @param waitingOnTaskId
-	 * 		not null
-	 *
-	 * @return the GraphBuilder instance itself
-	 */
-	public GraphBuilder<T> addTaskWaitsFor(T taskId, T waitingOnTaskId) {
-		synchronized (internalLock) {
-			Task<T> task = getOrAddTaskRepresentator(taskId);
-			Task<T> waitingOnTask = getOrAddTaskRepresentator(waitingOnTaskId);
-			task.addWaitFor(waitingOnTask);
-		}
-		return this;
-	}
+    /**
+     * Adds an edge between two tasks in the graph, if not yet present.
+     *
+     * @param taskId          not null
+     * @param waitingOnTaskId not null
+     * @return the GraphBuilder instance itself
+     */
+    public GraphBuilder<T> addTaskWaitsFor(T taskId, T waitingOnTaskId) {
+        synchronized (internalLock) {
+            Task<T> task = getOrAddTaskRepresentator(taskId);
+            Task<T> waitingOnTask = getOrAddTaskRepresentator(waitingOnTaskId);
+            task.addWaitFor(waitingOnTask);
+        }
+        return this;
+    }
 
-	/**
-	 * Creates a graph instance. It creates an unmodifiable "snapshot" of the current graph situation. The
-	 * graph-snapshot wont change, even if you continue to populate the graph with the same builder instance with the
-	 * current or another thread.
-	 * <p/>
-	 * Can be called as many times you want to create as many graph snapshots you want.
-	 *
-	 * @return Graph
-	 */
-	public Graph<T> build() {
-		final TreeSet<Task<T>> tasksSnapshotCopy;
-		synchronized (internalLock) {
-			final Collection<Task<T>> tasks = taskMap.values();
-			tasksSnapshotCopy = copy(tasks);
-		}
-		return new Graph<>(tasksSnapshotCopy);
-	}
+    /**
+     * Creates a graph instance. It creates an unmodifiable "snapshot" of the current graph situation. The
+     * graph-snapshot wont change, even if you continue to populate the graph with the same builder instance with the
+     * current or another thread.
+     * <p>
+     * Can be called as many times you want to create as many graph snapshots you want.
+     *
+     * @return Graph
+     */
+    public Graph<T> build() {
+        final TreeSet<Task<T>> tasksSnapshotCopy;
+        synchronized (internalLock) {
+            final Collection<Task<T>> tasks = taskMap.values();
+            tasksSnapshotCopy = copy(tasks);
+        }
+        return new Graph<>(tasksSnapshotCopy);
+    }
 
-	private Task<T> getOrAddTaskRepresentator(T taskId) {
-		Preconditions.checkArgumentNotNull(taskId, "taskId must not be null");
+    private Task<T> getOrAddTaskRepresentator(T taskId) {
+        Preconditions.checkArgumentNotNull(taskId, "taskId must not be null");
 
-		Task<T> taskRepresentator = taskMap.get(taskId);
-		if (taskRepresentator == null) {
-			taskRepresentator = new Task<>(taskId);
-			taskMap.put(taskId, taskRepresentator);
-		}
+        Task<T> taskRepresentator = taskMap.get(taskId);
+        if (taskRepresentator == null) {
+            taskRepresentator = new Task<>(taskId);
+            taskMap.put(taskId, taskRepresentator);
+        }
 
-		return taskRepresentator;
-	}
+        return taskRepresentator;
+    }
 
-	private static <T> TreeSet<Task<T>> copy(final Collection<Task<T>> originalTasks) {
-		Map<Task<T>, Task<T>> result = new LinkedHashMap<>(originalTasks.size());
-		recursiveCopy(originalTasks, result);
-		return new TreeSet<>(result.values());
-	}
+    private static <T> TreeSet<Task<T>> copy(final Collection<Task<T>> originalTasks) {
+        Map<Task<T>, Task<T>> result = new LinkedHashMap<>(originalTasks.size());
+        recursiveCopy(originalTasks, result);
+        return new TreeSet<>(result.values());
+    }
 
-	private static <T> void recursiveCopy(Collection<Task<T>> originalTasks, Map<Task<T>, Task<T>> result) {
-		for (Task<T> originalTask : originalTasks) {
-			Task<T> copiedTask = result.get(originalTask);
-			if (null == copiedTask) {
-				copiedTask = new Task<>(originalTask.getId());
-				result.put(originalTask, copiedTask);
-				recursiveCopy(originalTask.getWaitsForTasks(), result);
-				for (Task<T> originalWaitForTask : originalTask.getWaitsForTasks()) {
-					copiedTask.addWaitFor(result.get(originalWaitForTask));
-				}
-			}
-		}
-	}
+    private static <T> void recursiveCopy(Collection<Task<T>> originalTasks, Map<Task<T>, Task<T>> result) {
+        for (Task<T> originalTask : originalTasks) {
+            Task<T> copiedTask = result.get(originalTask);
+            if (null == copiedTask) {
+                copiedTask = new Task<>(originalTask.getId());
+                result.put(originalTask, copiedTask);
+                recursiveCopy(originalTask.getWaitsForTasks(), result);
+                for (Task<T> originalWaitForTask : originalTask.getWaitsForTasks()) {
+                    copiedTask.addWaitFor(result.get(originalWaitForTask));
+                }
+            }
+        }
+    }
 }
